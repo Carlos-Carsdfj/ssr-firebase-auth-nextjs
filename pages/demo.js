4133897133d3b9429d716f9804add7d578663360
carlos alberto ferreira <58960107+Carlos-Carsdfj@users.noreply.github.com>
2021-07-07 13:58:23 -0400

    Update demo.js

--- pages/demo.js
+++ pages/demo.js
@@ -1,12 +1,14 @@
+
 import {
-  AuthAction,
-  useAuthUser,
-  withAuthUser,
-  withAuthUserTokenSSR,
+  AuthAction,// Esto es usado para definir las acciones a tomar depediendo de la autenticación del cliente
+  useAuthUser,// Esto es una funcion que nos retornara la informacion del usuario actual tanto si esta autentificado como si no(Hace reserencia a use de Firebase)  
+  withAuthUser,//Esto es un Componente de orden superior o en sus siglas en ingles(HOC) que envuelve el componente y le dota de las props necesarias 
+  withAuthUserTokenSSR,//Esto es una funcion de orden superior que nos facilita usar el la autorizacion desde SSR
 } from 'next-firebase-auth'
 import firebase from 'firebase'
 
 const Demo = () => {
+  //Obtenemos el use del cliente para crear un renderizado condicional mas adelante
   const AuthUser = useAuthUser()
   return (
     <div>
@@ -16,7 +18,7 @@ const Demo = () => {
   )
 }
 
-// Note that this is a higher-order function.
+// Configuramos para redireccionar a la pagina de login si el usuario no esta autentificado  esto se aplica desde el servidor 
 export const getServerSideProps = withAuthUserTokenSSR({
   whenUnauthed: AuthAction.REDIRECT_TO_LOGIN
 })(() => {
@@ -24,7 +26,8 @@ export const getServerSideProps = withAuthUserTokenSSR({
     props: {}
   }
 })
-
+//Envolvemos Demo y exportamos por defecto el HOC asi agregamos la funcionalidad necesaria a la pagina a renderizar
 export default withAuthUser({
+  //Desde el cliente con la pagina ya renderizada redireccionamos si el usuario pierde la autenticación por alguna razon (cerrar sesion)
   whenUnauthedAfterInit: AuthAction.REDIRECT_TO_LOGIN
 })(Demo)
